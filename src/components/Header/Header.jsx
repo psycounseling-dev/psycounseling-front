6ddf2fc3b3e0a8cfe2816c099ea653b47e9547f7
v-pushkar <valerii.pushkar.work@gmail.conm>
2020-10-16 23:53:17 +0200

    dev init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import "./Header.scss"
+import React from "react";
+import "./Header.scss";
 const Header = (props) => {
-  return(
-  <header className="top-header">
-    <div className="top-header-titlerow">
-      <div className="top-header-logo">LOGO</div>
-      <div className="top-header-title">асоціація психологічного консультування та травмотерапії</div>
-      <div className="top-header-langnav">
-        <a href="" className="active">UA</a>
-        <a href="">EN</a>
+  return (
+    <header className="top-header">
+      <div className="top-header-titlerow">
+        <div className="top-header-logo">LOGO</div>
+        <div className="top-header-title">
+          асоціація психологічного консультування та травмотерапії
+        </div>
+        <div className="top-header-langnav">
+          <a href="" className="active">
+            UA
+          </a>
+          <a href="">EN</a>
+        </div>
       </div>
-    </div>
-    <nav className="top-nav">
-      <ul className="top-navigation">
-        <li>головна</li>
-        <li>про асоціацію</li>
-        <li>членство</li>
-        <li>кфлендар</li>
-        <li>новини</li>
-        <li>партнери</li>
-        <li>сертифікація</li>
-        <li>освіта</li>
-        <li>контакти</li>
-      </ul>
-    </nav>
-  </header>
-)};
-
+      <nav className="top-nav">
+        <ul className="top-navigation">
+          <li>головна</li>
+          <li>про асоціацію</li>
+          <li>членство</li>
+          <li>кфлендар</li>
+          <li>новини</li>
+          <li>партнери</li>
+          <li>сертифікація</li>
+          <li>освіта</li>
+          <li>контакти</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
