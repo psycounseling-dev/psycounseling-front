db21b8c4d30f7f9f29eb76fbdcc10ea3e6a7e657
v-pushkar <valerii.pushkar.work@gmail.conm>
2020-10-17 00:03:19 +0200

    Updates

---
+++ precache-manifest.45a722eec5ac7e6027a13cd3cccb3352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0425c131e0e231aab70feaf38632a3",
+    "url": "/psycounseling-front/index.html"
+  },
+  {
+    "revision": "14a9d4ae5e917bdc9284",
+    "url": "/psycounseling-front/static/css/main.98f64f6b.chunk.css"
+  },
+  {
+    "revision": "68e90faeae1f398781ff",
+    "url": "/psycounseling-front/static/js/2.dcbe675f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/psycounseling-front/static/js/2.dcbe675f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a9d4ae5e917bdc9284",
+    "url": "/psycounseling-front/static/js/main.29a1095f.chunk.js"
+  },
+  {
+    "revision": "c2fee972e8053ac6f3ac",
+    "url": "/psycounseling-front/static/js/runtime-main.0b912c60.js"
+  }
+]);
