1a0a7f68c7dad57c0a7e1f87c20a963c85fb4636
v-pushkar <valerii.pushkar.work@gmail.conm>
2020-10-16 23:50:41 +0200

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header/>
+        <h1>Асоціація психологічного консультування та травмотерапії</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
